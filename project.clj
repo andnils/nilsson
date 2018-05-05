2bb41c81c11a7c55e36c495cad4dbd143f8f2045
Anders Nilsson <anders.nilsson@sigma.se>
2018-05-05 22:56:16 +0200

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nilsson/lein-template "0.2.0"
+(defproject nilsson/lein-template "0.2.1-SNAPSHOT"
   :description "A leiningen template for a clojure backend with postgresql database, and ES6+React frontend."
   :url "https://github.com/andnils/nilsson"
   :license {:name "Eclipse Public License"
