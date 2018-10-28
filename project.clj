7c0b4124a8d1348ff14dbd790746ac2055b81f2b
Anders Nilsson <andnils@gmail.com>
2018-10-28 22:30:31 +0100

    update repos

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject nilsson/lein-template "0.4.5"
+(defproject nilsson/lein-template "0.4.6-SNAPSHOT"
   :description "A leiningen template for a clojure backend with postgresql database, and ES6+React frontend."
   :url "https://github.com/andnils/nilsson"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/andnils/nilsson"}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
   :eval-in-leiningen true)
